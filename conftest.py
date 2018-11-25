fa56aee977bc802b3d09da8a2cdaef1ce1c4233a
Alexander Serebrov <serebrov.alexandr@gmail.com>
2018-11-25 23:09:53 +0200

    Add database connection, migrations, models

--- conftest.py
+++ conftest.py
@@ -8,14 +8,14 @@ from app import app
 
 @pytest.fixture
 def client():
-    # db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    # db_fd, app.config['DATABASE'] = tempfile.mkstemp()
     app.config.from_object(os.environ['APP_SETTINGS'])
     client = app.test_client()
 
-    # with flaskr.app.app_context():
-    #     flaskr.init_db()
+    # with app.app_context() as context:
+        # context.init_db()
 
     yield client
 
     # os.close(db_fd)
-    # os.unlink(flaskr.app.config['DATABASE'])
+    # os.unlink(app.config['DATABASE'])

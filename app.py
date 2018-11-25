05a2cb4830733c8c2b1aa91a21d2b81e203c767c
Alexander Serebrov <serebrov.alexandr@gmail.com>
2018-11-25 19:01:14 +0200

    Add configuration from env variable to app

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -10,6 +13,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
